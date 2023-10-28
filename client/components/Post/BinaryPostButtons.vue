44c34f76d0847bb98bc09286d755c84d368f0dea
Lauren Wong <laurensw@mit.edu>
2023-10-28 00:45:38 -0400

    delete comment

--- client/components/Post/BinaryPostButtons.vue
+++ client/components/Post/BinaryPostButtons.vue
@@ -25,10 +25,6 @@ onBeforeMount(() => {
     <a :class="{ selected: mode === 'following' }" v-on:click="viewFollowing">Following</a>
     <a :class="{ selected: mode === 'all' }" v-on:click="viewAll">All</a>
   </div>
-  <!-- <div class="pure-button-group" role="group">
-    <button class="pure-button" :class="{ selected: mode === 'following' }" v-on:click="viewFollowing">Following</button>
-    <button class="pure-button" :class="{ selected: mode === 'all' }" v-on:click="viewAll">All</button>
-  </div> -->
 </template>
 
 <style scoped>
